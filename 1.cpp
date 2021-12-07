7dc4ed72bb91dee5116956c6e097700a42bb7c68
Aadarsh Bishen <47295927+bishen28@users.noreply.github.com>
2021-12-07 23:38:12 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,49 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string smallest(string s){
+    
+    int l = s.length();
+    string ans = "";
 
-void findall(int n, string osf, string &str, vector<string> &ans, int k){
+    for (int i = 0; i < l-1; i++) {
 
-     if(osf.length()==k){
-        ans.push_back(osf);
-        return;
-     }
-     for(int i=0;i<n;i++){
-        findall(n,osf+str[i],str,ans,k);
-     }
+        if (s[i] > s[i + 1]) {
+            for (int j = 0; j < l; j++) {
+                if (i != j)
+                    ans += s[j];
+            }
+            return ans;
+        }
+    }
+    ans = s.substr(0, l - 1);
+    return ans;
 }
 
-
-
-void solve(){
-
-   int n,k; cin>>n;
-   string str;
-   for(int i=0;i<n;i++){
-      string s;cin>>s;
-      str+=s;
-   }
-   cin>>k;
-   vector<string> ans;
-   string osf = "";
-   findall(n,osf,str,ans,k);
-
-   sort(ans.begin(),ans.end());
-
-   for(int i=0;i<ans.size();i++){
-     cout << ans[i];
-     if(i<ans.size()-1) cout << endl;
-   }
-
-}
 
 int main(){
-  
-    ios_base::sync_with_stdio(0) ;
-    cin.tie(0) ; cout.tie(0) ;
-    cout.precision(20);
 
-    solve();
-  
+     string s;
+     cin>>s;
+    
+    cout << smallest(s);
+
     return 0;
 }
